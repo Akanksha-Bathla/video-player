4c8e3a45d6403ee26a6d0a2f9b8b8c676e776775
Akanksha-Bathla <akankshabathla.18.22.26@gmail.com>
2025-01-22 19:03:04 +0530

    progress bar updated and timestamp

--- script.js
+++ script.js
@@ -26,15 +26,24 @@ function updatePlayIcon() {
 
 //update progress and timestamp
 function updateProgress() {
-    console.log('hello world');
-    console.log(`Current Time: ${video.currentTime}`);
-    console.log(`Duration: ${video.duration}`);
     progress.value = (video.currentTime / video.duration) * 100;
+    //get minutes
+    let mins = Math.floor(video.currentTime / 60);
+    if(mins < 10){
+        mins = '0' + String(mins);
+    }
+
+    let secs = Math.floor(video.currentTime % 60);
+    if(secs < 10){
+        secs = '0' + String(secs);
+    }
+
+    timestamp.innerHTML = `${mins}:${secs}`;
 }
 
 //set video time to progress
 function setVideoProgress() {
-    return true;
+    video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 //stop video
@@ -47,7 +56,7 @@ function stopVideo() {
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('pause', updatePlayIcon);
 video.addEventListener('play', updatePlayIcon);
-video.addEventListener('timestamp', updateProgress);
+video.addEventListener('timeupdate', updateProgress);
 
 play.addEventListener('click', toggleVideoStatus);
 
